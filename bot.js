6524d9af522b255608de7a01a5b9a83a3c741696
mohamed lachachi <34174741+Lachachi-Web@users.noreply.github.com>
2025-11-07 22:03:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 import TelegramBot from "node-telegram-bot-api";
 
 // نبدلو هذا بالتوكن الحقيقي تاع البوت ديالك
-const token = 8573042484:AAFvkKY-Um5yFeCgK_cpzXRRrZTVktPw6yw;
+const token = "8573042484:AAFvkKY-Um5yFeCgK_cpzXRRrZTVktPw6yw";
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "🚀 البوت شغال بنجاح ✅");
 });
+
